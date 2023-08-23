235e19d165d8f69ecbdcccee77e86868d2325cc4
Programmer Sanket <55836873+DevSanket@users.noreply.github.com>
2023-08-23 10:29:35 +0530

    Camera Change

--- components/CameraCapture/index.jsx
+++ components/CameraCapture/index.jsx
@@ -1,8 +1,8 @@
-// src/components/CameraCapture.js
 import { useState } from "react";
+import Webcam from "react-webcam";
 
 const CameraCapture = ({ pickedImages, setPickedImages }) => {
-  const [facingMode, setFacingMode] = useState("environment"); // 'environment' for back camera, 'user' for front camera
+  const [facingMode, setFacingMode] = useState("environment");
 
   const handleSwitchCamera = () => {
     setFacingMode((prevFacingMode) =>
@@ -10,31 +10,16 @@ const CameraCapture = ({ pickedImages, setPickedImages }) => {
     );
   };
 
-  const handleCapture = async () => {
-    if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
-      console.log("getUserMedia is not supported");
-      return;
-    }
-
-    try {
-      const stream = await navigator.mediaDevices.getUserMedia({ video: true });
-      const video = document.getElementById("camera-feed");
-      video.srcObject = stream;
-    } catch (error) {
-      console.error("Error accessing the camera:", error);
-    }
-  };
-
   const handleCaptureImage = async () => {
-    try {
-      const constraints = {
-        video: {
-          facingMode, // Use the currently selected facing mode
-          width: { ideal: 1280 }, // Set preferred width if needed
-          height: { ideal: 720 }, // Set preferred height if needed
-        },
-      };
+    const constraints = {
+      video: {
+        facingMode,
+        width: { ideal: 1280 },
+        height: { ideal: 720 },
+      },
+    };
 
+    try {
       const stream = await navigator.mediaDevices.getUserMedia(constraints);
       const video = document.getElementById("camera-feed");
       video.srcObject = stream;
@@ -65,17 +50,19 @@ const CameraCapture = ({ pickedImages, setPickedImages }) => {
 
   return (
     <div className="space-y-3">
-      <video id="camera-feed" autoPlay style={{ display: "block" }}></video>
+      <Webcam
+        id="camera-feed"
+        audio={false}
+        videoConstraints={{ facingMode }}
+        style={{ display: "block" }}
+      />
+
       <div onClick={handleSwitchCamera} className="btn-theme-5 w-full">
         <span className="w-full">Switch Camera</span>
       </div>
       <div className="flex flex-row gap-5">
-        <div className="btn-theme-5 w-full" onClick={handleCapture}>
-          <span className="w-full">कॅमेरा चालू करा</span>
-        </div>
-
         <div onClick={handleCaptureImage} className="btn-theme-5 w-full">
-          <span className="w-full">फोटो कॅप्चर करा</span>
+          <span className="w-full">Capture Photo</span>
         </div>
       </div>
     </div>
